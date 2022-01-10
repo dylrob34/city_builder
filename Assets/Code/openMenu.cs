ab91b8fa89a8b1943761796082710b79f74685eb
BradyAnderson42 <57693633+BradyAnderson42@users.noreply.github.com>
2022-01-09 22:08:49 -0500

    Moving camera is now right mouse click. Menu opens and closes. Placing objects kinda works ish but ill fix that later. Im going to bed

--- Assets/Code/openMenu.cs
+++ Assets/Code/openMenu.cs
@@ -10,13 +10,23 @@ public class openMenu : MonoBehaviour
     public GameObject menu;
     public Image thisImage;
     public Sprite exitSprite;
+    public Sprite defaultSprite;
 
     // Start is called before the first frame update
+    [System.Obsolete]
     public void enableMenu(bool i)
     {
         //GameObject menu = GameObject.Find("Menu");
-        menu.SetActive(i);
-        thisImage.sprite = exitSprite;
+        if (menu.active == false)
+        {
+            menu.SetActive(true);
+            thisImage.sprite = exitSprite;
+        }
+        else if (menu.active)
+        {
+            menu.SetActive(false);
+            thisImage.sprite = defaultSprite;
+        }
     }
 
 
--- Assets/Code/placeObject.cs
+++ Assets/Code/placeObject.cs
@@ -6,15 +6,43 @@ using UnityEngine.UI;
 public class placeObject : MonoBehaviour
 {
 
-    
+    public GameObject blackBuilding, redBuilding, blueBuilding;
+    public bool black, red, blue;
 
     // Start is called before the first frame update
-    void start()
+    public void Place(int i)
     {
+        black = false;
+        red = false;
+        blue = false;
         
+        
+        if (i == 1)
+        {
+            black = true;
+        }
+        if (i == 2)
+        {
+            Instantiate(redBuilding, Input.mousePosition, new Quaternion(0, 0, 0, 0));
+        }
+        if (i == 3)
+        {
+            Instantiate(blueBuilding, Input.mousePosition, new Quaternion(0, 0, 0, 0));
+        }
+        Debug.Log(i.ToString());
     }
 
-    
+    private void Update()
+    {
+        if (black)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Instantiate(blackBuilding, new Vector3(0, 5, 0), new Quaternion(0, 0, 0, 0));
+                black = false;
+            }
+        }
+    }
 
-    
+
 }
